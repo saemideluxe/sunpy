1ab154e7e6bf6f585e923d9bf1ee07f839edb7b8
Samuel Stachelski <samuel.vonstachelski@fhnw.ch>
2014-09-18 10:35:50 +0200

    setup package structure for hespe package

---
+++ sunpy/net/hespe/__init__.py
@@ -0,0 +1,5 @@
+﻿from __future__ import absolute_import
+
+#from sunpy.net.hespe.hespe import ??
+
+#__all__ = ['??']
---
+++ sunpy/net/hespe/hespe.py
@@ -0,0 +1,12 @@
+﻿# -*- coding: utf-8 -*-
+# Author: Samuel Stachelski <samuel.vonstachelsi@fhnw.ch>
+#
+#
+
+from __future__ import absolute_import
+from __future__ import division
+
+"""
+This module provides access to the HESPE database.
+"""
+
