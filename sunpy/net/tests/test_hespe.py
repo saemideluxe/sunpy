8f34f2958940a14ee27f259368bed1cee32a6a3f
Samuel Stachelski <samuel.vonstachelski@fhnw.ch>
2014-09-19 14:56:27 +0200

    Added testfile, still empty

---
+++ sunpy/net/tests/test_hespe.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Author: Samue Stachelski <samuel.vonstachelski@fhnw.ch>
+
+from __future__ import absolute_import
+
+import datetime
+import pytest
+
+from sunpy.net import hespe
+
